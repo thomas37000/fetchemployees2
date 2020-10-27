0f9ecaf75025ec0eca963514f4986637ee70e91a
thomas <thomas.chalanson@sfr.fr>
2020-10-27 09:28:45 +0100

    put a quote by default

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+const firstQuote = [
+  {
+    quote:
+      "Shoplifting is a victimless crime, like punching someone in the dark.",
+    character: "Nelson Muntz",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+  }
+];
+
 class Quotes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      simpson: ""
+      simpson: firstQuote[0]
     };
     this.getSimpson = this.getSimpson.bind(this);
   }
